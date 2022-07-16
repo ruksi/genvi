0a37531a632748319b3e9384d5708730a07ae18b
ruksi <me@ruk.si>
2022-07-16 03:42:12 +0300

    Make venv check work with Python 2
    Many systems might still have that as the default...

--- is_virtualenv.py
+++ is_virtualenv.py
@@ -1,10 +1,11 @@
 import sys
 
 # Tools for checking if Python process is in a virtual environment.
+# This is intentionally missing type hints for it to work on Python 2.
 # Source: https://stackoverflow.com/a/1883251
 
 
-def get_base_prefix_compat() -> str:
+def get_base_prefix_compat():  # type: ignore[no-untyped-def]
     return (
         getattr(sys, 'base_prefix', None)
         or getattr(sys, 'real_prefix', None)
@@ -12,9 +13,9 @@ def get_base_prefix_compat() -> str:
     )
 
 
-def is_virtualenv() -> bool:
-    return get_base_prefix_compat() != sys.prefix
+def is_virtualenv():  # type: ignore[no-untyped-def]
+    return get_base_prefix_compat() != sys.prefix  # type: ignore[no-untyped-call]
 
 
 if __name__ == '__main__':
-    print(is_virtualenv())
+    print(is_virtualenv())  # type: ignore[no-untyped-call]
--- is_virtualenv_test.py
+++ is_virtualenv_test.py
@@ -4,4 +4,4 @@ from is_virtualenv import is_virtualenv
 def test_for_smoke() -> None:
     # tedious to test something that check environment status
     # just do a smoke test that it runs and seems consistent
-    assert is_virtualenv() or not is_virtualenv()
+    assert is_virtualenv() or not is_virtualenv()  # type: ignore[no-untyped-call]
