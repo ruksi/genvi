c03656e8fa11b38cbc486539057e476fdc98f99c
ruksi <me@ruk.si>
2022-07-17 04:41:43 +0300

    Refactor example `main()`
    * Make it receive no parameters
    * Wrap it to catch and report exceptions

--- myproject/console/executable.py
+++ myproject/console/executable.py
@@ -1,15 +1,22 @@
 import sys
-from typing import Optional, TextIO
+from typing import List
 
-ErrorCode = int
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+ErrorCode = Literal[0, 1]
 
 
-def main(text_io: Optional[TextIO] = None) -> ErrorCode:
-    if not text_io:
-        text_io = sys.stdout
-    text_io.write(f'mock command line interface: {sys.argv}\n')
+def main() -> ErrorCode:
+    try:
+        return run(sys.argv)
+    except Exception as e:  # pylint: disable=broad-except
+        sys.stderr.write(f'{e}\n')
+        return 1
+
+
+def run(arguments: List[str]) -> ErrorCode:
+    sys.stdout.write(f'mock command line interface: {arguments}\n')
     return 0
-
-
-if __name__ == '__main__':
-    sys.exit(main())
--- myproject/console/executable_test.py
+++ myproject/console/executable_test.py
@@ -1,19 +1,20 @@
-import io
-
 from _pytest.capture import CaptureFixture
+from pytest_mock import MockFixture
 
 from myproject.console.executable import main
 
 
-def test_executable_default(capsys: CaptureFixture[str]) -> None:
+def test_main_success(capsys: CaptureFixture[str]) -> None:
     assert main() == 0
-    captured = capsys.readouterr()
-    assert 'mock command line interface:' in captured.out
+    standard = capsys.readouterr()
+    assert 'mock command line interface:' in standard.out
+    assert '' == standard.err
 
 
-def test_executable_custom_file() -> None:
-    string_io = io.StringIO()
-    assert main(text_io=string_io) == 0
-    string_io.seek(0)
-    output = string_io.read()
-    assert 'mock command line interface:' in output
+def test_main_failure(capsys: CaptureFixture[str], mocker: MockFixture) -> None:
+    msg = 'boom things went wrong'
+    mocker.patch('myproject.console.executable.run').side_effect = Exception(msg)
+    assert main() == 1
+    standard = capsys.readouterr()
+    assert '' == standard.out
+    assert f'{msg}\n' == standard.err
