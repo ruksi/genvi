273e7f430de505932174432dfd7679ae8aa0d4a7
ruksi <me@ruk.si>
2022-07-21 22:48:31 +0300

    Change `magic` config to a namespace
    dataclasses are generally nicer but makes more
    sense in the current context

--- magic/console/config.py
+++ magic/console/config.py
@@ -1,14 +1,12 @@
 import string
-from argparse import ArgumentParser
-from dataclasses import dataclass
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import List
 
 from magic.utils.errors import InternalError, ValidationError
 
 
-@dataclass
-class Config:
+class Config(Namespace):
     name: str
     author: str
     email: str
@@ -26,13 +24,14 @@ class Config:
                 'package name must consist of lowercase ASCII'
                 f' ({string.ascii_lowercase})',
             )
-        if self.name in {'images', 'magic', 'tests'}:
+        if self.name in {'images', 'magic', 'tests', 'tools', 'dist', 'build'}:
             raise ValidationError('package name is invalid')
 
     def _validate_root(self) -> None:
+        # do a few sanity checks...
         if not self.genvi_root.is_dir():
             raise InternalError('package root must be an existing directory')
-        if not Path(self.genvi_root, 'Makefile').is_file():  # sanity check
+        if not Path(self.genvi_root, 'Makefile').is_file():
             raise InternalError('package root does not look like `genvi` root')
 
 
@@ -41,26 +40,23 @@ def parse_config(arguments: List[str]) -> Config:
     parser.add_argument('--name', help='package name')
     parser.add_argument('--author', help='package author name')
     parser.add_argument('--email', help='package author email')
-    parguments = parser.parse_args(args=arguments)
-
-    if not parguments.name:
-        parguments.name = input('Package name: ')
-    if not parguments.author:
-        parguments.author = input('Author name: ')
-    if not parguments.email:
-        parguments.email = input('Author email: ')
-
-    if not parguments.author:
-        parguments.author = 'Arthur Author'
-    if not parguments.email:
-        parguments.email = 'author@example.com'
-
-    config = Config(
-        name=parguments.name,
-        author=parguments.author,
-        email=parguments.email,
-        genvi_root=resolve_genvi_root(),
+    config = parser.parse_args(
+        args=arguments,
+        namespace=Config(genvi_root=resolve_genvi_root()),
     )
+
+    if not config.name:
+        config.name = input('Package name: ')
+    if not config.author:
+        config.author = input('Author name: ')
+    if not config.email:
+        config.email = input('Author email: ')
+
+    if not config.author:
+        config.author = 'Arthur Author'
+    if not config.email:
+        config.email = 'author@example.com'
+
     config.validate()
     return config
 
