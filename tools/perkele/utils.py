41750ee29937d6b5eba813042d445d2390d076f5
ruksi <me@ruk.si>
2022-08-02 22:19:10 +0300

    Make `pop_until` also return the popped values

--- tools/perkele/utils.py
+++ tools/perkele/utils.py
@@ -3,6 +3,8 @@ from typing import Callable, List, TypeVar
 T = TypeVar('T')
 
 
-def pop_until(items: List[T], condition: Callable[[T], bool]) -> None:
+def pop_until(items: List[T], condition: Callable[[T], bool]) -> List[T]:
+    popped: List[T] = []
     while items and not condition(items[-1]):
-        items.pop()
+        popped.append(items.pop())
+    return popped
--- tools/perkele/utils_test.py
+++ tools/perkele/utils_test.py
@@ -5,17 +5,20 @@ from tools.perkele.utils import pop_until
 
 def test_noop() -> None:
     items = ['', 'one', 'two', '', 'three']
-    pop_until(items, lambda item: bool(item != ''))
+    popped = pop_until(items, lambda item: bool(item != ''))
     assert items == ['', 'one', 'two', '', 'three']
+    assert not popped
 
 
 def test_initially_empty() -> None:
     items: List[str] = []
-    pop_until(items, lambda item: bool(item != ''))
+    popped = pop_until(items, lambda item: bool(item != ''))
     assert not items
+    assert not popped
 
 
 def test_popped_empty() -> None:
     items: List[str] = ['', '', '']
-    pop_until(items, lambda item: bool(item != ''))
+    popped = pop_until(items, lambda item: bool(item != ''))
     assert not items
+    assert popped == ['', '', '']
