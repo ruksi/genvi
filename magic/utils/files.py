7fbf5776f539a7f797ce8099bc65620dcc3c4687
ruksi <me@ruk.si>
2022-11-12 02:11:16 +0200

    ♻️ Refactor magic utils files `Path` usage

--- magic/utils/files.py
+++ magic/utils/files.py
@@ -7,31 +7,29 @@ from magic.utils.consts import DEFAULT_AUTHOR, DEFAULT_EMAIL, DEFAULT_PACKAGE_NA
 
 
 def rename_package(config: Config) -> None:
-    package_dir = Path(config.genvi_root, DEFAULT_PACKAGE_NAME)
-    test_dir = Path(config.genvi_root, 'tests')
+    package_dir = config.genvi_root / DEFAULT_PACKAGE_NAME
+    test_dir = config.genvi_root / 'tests'
     files_to_search_and_replace = (
         list(package_dir.glob('**/*.py'))
         + list(test_dir.glob('**/*.py'))
         + [
-            Path(config.genvi_root, '.github/workflows/ci.yml'),
-            Path(config.genvi_root, 'LICENSE'),
-            Path(config.genvi_root, 'MANIFEST.in'),
-            Path(config.genvi_root, 'Makefile'),
-            Path(config.genvi_root, 'pyproject.toml'),
-            Path(config.genvi_root, 'setup.cfg'),
+            (config.genvi_root / '.github/workflows/ci.yml'),
+            (config.genvi_root / 'LICENSE'),
+            (config.genvi_root / 'MANIFEST.in'),
+            (config.genvi_root / 'Makefile'),
+            (config.genvi_root / 'pyproject.toml'),
+            (config.genvi_root / 'setup.cfg'),
         ]
     )
 
-    for filename in files_to_search_and_replace:
-        with Path(filename).open(encoding='utf-8') as file:
-            contents = file.read()
-        with Path(filename).open(encoding='utf-8', mode='wt') as file:
-            contents = contents.replace(DEFAULT_PACKAGE_NAME, config.name)
-            if config.author:
-                contents = contents.replace(DEFAULT_AUTHOR, config.author)
-            if config.email:
-                contents = contents.replace(DEFAULT_EMAIL, config.email)
-            file.write(contents)
+    for file_path in files_to_search_and_replace:
+        contents = file_path.read_text(encoding='utf-8')
+        contents = contents.replace(DEFAULT_PACKAGE_NAME, config.name)
+        if config.author:
+            contents = contents.replace(DEFAULT_AUTHOR, config.author)
+        if config.email:
+            contents = contents.replace(DEFAULT_EMAIL, config.email)
+        file_path.write_text(contents, encoding='utf-8')
 
     shutil.move(
         str(package_dir),
@@ -43,11 +41,10 @@ def generate_readme(config: Config) -> str:
     substitutes = {
         'name': config.name,
     }
-    with Path(Path(__file__).parent, 'readme_template.md').open(
-        encoding='utf-8',
-    ) as file:
-        tmpl = Template(file.read())
-        return tmpl.substitute(substitutes)
+    tmpl_path = Path(Path(__file__).parent, 'readme_template.md')
+    tmpl_str = tmpl_path.read_text(encoding='utf-8')
+    tmpl = Template(tmpl_str)
+    return tmpl.substitute(substitutes)
 
 
 def strip_lines_between_markers(target: Path, start: str, end: str) -> None:
@@ -62,5 +59,4 @@ def strip_lines_between_markers(target: Path, start: str, end: str) -> None:
                 continue
             if keeping:
                 lines.append(line)
-    with target.open(encoding='utf-8', mode='w') as file:
-        file.writelines(lines)
+    target.write_text(''.join(lines), encoding='utf-8')
