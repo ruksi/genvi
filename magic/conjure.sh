69490ca2c4a0afc76b963d7b7876cb659761a2e8
ruksi <me@ruk.si>
2023-01-18 08:50:05 +0200

    🧑‍💻 Don't use `make` in the initial setup

--- magic/conjure.sh
+++ magic/conjure.sh
@@ -5,13 +5,10 @@ set -euo pipefail
 
 # these version checks will stop the script if missing the dependency
 git --version
-make --version
 python --version
 
 echo "Creating a new directory for a Python project."
-read -r -p "Package Name (lowercase letters, no special characters of any kind): " PACKAGE < /dev/tty
-read -r -p "Author Name: " AUTHOR < /dev/tty
-read -r -p "Author Email: " EMAIL < /dev/tty
-git clone git@github.com:ruksi/genvi.git "$PACKAGE"
-cd "$PACKAGE"
-make package="$PACKAGE" author="$AUTHOR" email="$EMAIL"
+read -r -p "Project name (also the lowercase name of the directory): " PROJECT < /dev/tty
+git clone git@github.com:ruksi/genvi.git "$PROJECT"
+cd "$PROJECT"
+python -m magic
